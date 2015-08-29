94e094a0beda9f3de622294fc57de1d98bbd329b
Steven <qzs2147@gmail.com>
2015-08-29 14:31:58 +0800

    增加版本检测功能

--- ios/Quick-Accounting/Main/ViewController.m
+++ ios/Quick-Accounting/Main/ViewController.m
@@ -7,6 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <AFNetworking.h>
+#import <NSObjectExtend.h>
+#import <BlocksKit.h>
+
+/**
+ *  检测新版本的URL
+ */
+#define CHECK_NEW_VERSION_URL @"https://github.com/qzs21/Quick-Accounting/raw/master/config/version.json"
 
 @interface ViewController ()
 
@@ -25,6 +33,38 @@
                  toViewControllerKey:@"AddAccountingViewController"
                                param:nil
                             animated:UINavigationControllerAnimatedNone];
+    
+    
+    [[AFHTTPRequestOperationManager manager] GET:CHECK_NEW_VERSION_URL parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        if ([responseObject isKindOfClass:NSDictionary.class])
+        {
+            NSDictionary * data = responseObject;
+            
+            NSString * build = data[@"base"][@"build"];
+            NSString * version = data[@"base"][@"version"];
+            NSString * info = data[@"base"][@"info"];
+            NSString * url = data[@"base"][@"url"];
+            
+            NSUInteger currentVersion = [[UIDevice appBuildVersion] integerValue];
+            NSUInteger newVersion = [build integerValue];
+            if (newVersion > currentVersion && url.length)
+            {
+                [[UIAlertView bk_showAlertViewWithTitle:@"有新版本更新哦！"
+                                                message:[NSString stringWithFormat:@"%@(build:%@)\n%@", version, build, info]
+                                      cancelButtonTitle:@"取消"
+                                      otherButtonTitles:@[@"更新"]
+                                                handler:^(UIAlertView *alertView, NSInteger buttonIndex)
+                {
+                    if (buttonIndex)
+                    {
+                        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
+                    }
+                }] show];
+                
+            }
+        }
+    } failure:nil];
 }
 
 @end
